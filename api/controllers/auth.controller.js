2df6454ae458e7426ef1a1ca74bd478716b02b45
ILakshitha <140165538+ILakshitha@users.noreply.github.com>
2024-06-15 09:00:29 +0530

    add error for signup route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -10,7 +10,7 @@ export const signup = async(req,res)=>{
         await newUser.save();
     res.status(201).json('user create successfully!');
     } catch (error) {
-        res.status(500).json(error);
+        res.status(500).json(error.message);
     }
     
 }
