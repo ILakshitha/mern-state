c02ebafef9bd885702ab8197176496e3fa60b04d
ILakshitha <140165538+ILakshitha@users.noreply.github.com>
2024-06-16 11:14:06 +0530

    Add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import {Link, useNavigate} from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { signInStart,signInSuccess,signInFailure } from '../redux/user/userSlice';
 
 export default function SignIn() {
   const [formData,setFormData] =useState({});
-  const [error,setError]= useState(null);
-  const [loading, setLoading] = useState(false);
+  const {loading,error }= useSelector((state)=>state.user);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
 
   const handleChange = (e) => {
@@ -15,10 +17,10 @@ export default function SignIn() {
     });
   };
 
-  const handleSubmit = async(e)=>{
-    e.preventDefault()
-
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     try {
+      dispatch(signInStart());
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
         headers: {
@@ -28,26 +30,16 @@ export default function SignIn() {
       });
       const data = await res.json();
       console.log(data);
-  
-      if(data.success === false){
-        setError(data.message);
-        setLoading(false);
-
+      if (data.success === false) {
+        dispatch(signInFailure(data.message));
         return;
       }
-      setLoading(false);
-      setError(null);
+      dispatch(signInSuccess(data));
       navigate('/');
     } catch (error) {
-      setLoading(false);
-      setError(error.message);
-      
+      dispatch(signInFailure(error.message));
     }
-
-    
-
-    
-  }
+  };
   console.log(formData);
   return (
     <div className='p-3 max-w-lg mx-auto'>
@@ -69,8 +61,11 @@ export default function SignIn() {
         id='password' 
         onChange={handleChange}/>
 
-        <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-70'>
-        {loading ? 'Loading...' : 'Sign In'}
+<button
+          disabled={loading}
+          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
+        >
+          {loading ? 'Loading...' : 'Sign In'}
         </button>
       </form>
       <div className='flex gap-2 mt-5'>
